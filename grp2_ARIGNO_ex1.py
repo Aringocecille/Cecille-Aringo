6c70fc6c974b5393d7bb2117af60257dcabdbbdf
Cecille Aringo <144106783+Aringocecille@users.noreply.github.com>
2024-08-31 09:05:25 +0800

    Create grp2_ARIGNO_ex1.py

---
+++ grp2_ARIGNO_ex1.py
@@ -0,0 +1,36 @@
+
+# My first simple script
+print("Hello, I'm Aringo Cecille J.! BSCS 3F")
+
+# Variables
+num = 123
+name = "Cecille J. Aringo"
+print("Number:", num)
+print("Name:", name)
+
+# Variable Names
+# a. Create 4 variable names using the first names of your group members.
+meriel = "meriel"
+kath = "kath"
+mohanida = "mohanida"
+jonalyn = "jonalyn"
+
+# Print the values of the group member variables
+# Variable Names
+print("Group Member First Names:")
+print(meriel)
+print(kath)
+print(mohanida)
+print(jonalyn)
+
+# Many Values to Multiple Variables
+x, y, z = 1, 2, 3
+print("x:", x)
+print("y:", y)
+print("z:", z)
+
+# One Value to Multiple Variables
+a = b = c = 15
+print("a:", a)
+print("b:", b)
+print("c:", c)
